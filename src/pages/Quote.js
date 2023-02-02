7fe91ea898722a12e73bde493463d2a6533e1591
Patrick Mukula <Patrickkeita80@yahoo.com>
2023-02-02 12:26:27 -0800

    Add Quote.js file

---
+++ src/pages/Quote.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Quote = () => (
+  <div className="quote">
+    <p className="quote-page-content">
+      &ldquo;The ends always justify the means.&rdquo; - Nicolo  Makiavely
+    </p>
+  </div>
+);
+
+export default Quote;
